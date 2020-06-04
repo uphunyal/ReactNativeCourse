9e7d18ae79387cf2f89a18c8a4afd5f1dad4a777
Utsav <phunyal.utsav1@gmail.com>
2020-06-03 20:21:03 -0500

    Update yelp.js

--- YelpSearch/src/api/yelp.js
+++ YelpSearch/src/api/yelp.js
@@ -3,6 +3,5 @@ import axios from 'axios'
 export default axios.create({
     baseURL: 'https://api.yelp.com/v3/businesses', 
     headers: {
-        Authorization: 'Bearer NVWZtD6NWvMiDc4bAlvxH93QQQSZ54GraKpewotokDGSC7meBetk3IE4YsuKpTBCJtBRWn-9R7msi1DWdsxRHRQvVhDR2oPIyI27lPGR9hqrGAR3KNs29Bu39TrTXnYx'
-    }
+        Authorization: 'Bearer ENTER_API_KEY_HERE }
 });
